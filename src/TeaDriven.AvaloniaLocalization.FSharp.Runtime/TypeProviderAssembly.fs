8295aabee80df8bb182dbe7099b431141b32349f
TeaDrivenDev <TeaDrivenDev@gmail.com>
2026-02-19 00:07:09 +0100

    Remove separate runtime project

--- src/TeaDriven.AvaloniaLocalization.FSharp.Runtime/TypeProviderAssembly.fs
+++
@@ -1,7 +0,0 @@
-namespace TeaDriven.AvaloniaLocalization.FSharp.Runtime
-
-open FSharp.Core.CompilerServices
-
-// Put the TypeProviderAssemblyAttribute in the runtime DLL, pointing to the design-time DLL
-[<assembly: TypeProviderAssembly("TeaDriven.AvaloniaLocalization.FSharp.DesignTime")>]
-do ()
