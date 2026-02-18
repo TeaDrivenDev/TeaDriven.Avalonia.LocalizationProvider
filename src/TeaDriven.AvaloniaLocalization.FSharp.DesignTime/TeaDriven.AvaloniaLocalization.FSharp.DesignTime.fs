5146d3358949324242b9c7a816116a3d6142b7f1
TeaDrivenDev <TeaDrivenDev@gmail.com>
2026-02-18 22:59:32 +0100

    Implement basic functionality

--- src/TeaDriven.AvaloniaLocalization.FSharp.DesignTime/TeaDriven.AvaloniaLocalization.FSharp.DesignTime.fs
+++ src/TeaDriven.AvaloniaLocalization.FSharp.DesignTime/TeaDriven.AvaloniaLocalization.FSharp.DesignTime.fs
@@ -4,6 +4,8 @@ open System
 open System.Collections.Generic
 open System.IO
 open System.Reflection
+open System.Xml.Linq
+
 open FSharp.Quotations
 open FSharp.Core.CompilerServices
 open MyNamespace
@@ -23,7 +25,7 @@ type BasicErasingProvider (config : TypeProviderConfig) as this =
     let asm = Assembly.GetExecutingAssembly()
 
     // check we contain a copy of runtime files, and are not referencing the runtime DLL
-    do assert (typeof<DataSource>.Assembly.GetName().Name = asm.GetName().Name)  
+    do assert (typeof<DataSource>.Assembly.GetName().Name = asm.GetName().Name)
 
     let createTypes () =
         let myType = ProvidedTypeDefinition(asm, ns, "MyType", Some typeof<obj>)
@@ -69,7 +71,7 @@ type BasicGenerativeProvider (config : TypeProviderConfig) as this =
     let asm = Assembly.GetExecutingAssembly()
 
     // check we contain a copy of runtime files, and are not referencing the runtime DLL
-    do assert (typeof<DataSource>.Assembly.GetName().Name = asm.GetName().Name)  
+    do assert (typeof<DataSource>.Assembly.GetName().Name = asm.GetName().Name)
 
     let createType typeName (count:int) =
         let asm = ProvidedAssembly()
@@ -81,7 +83,7 @@ type BasicGenerativeProvider (config : TypeProviderConfig) as this =
         let ctor2 = ProvidedConstructor([ProvidedParameter("InnerState", typeof<string>)], invokeCode = fun args -> <@@ (%%(args.[1]):string) :> obj @@>)
         myType.AddMember(ctor2)
 
-        for i in 1 .. count do 
+        for i in 1 .. count do
             let prop = ProvidedProperty("Property" + string i, typeof<int>, getterCode = fun args -> <@@ i @@>)
             myType.AddMember(prop)
 
@@ -91,10 +93,58 @@ type BasicGenerativeProvider (config : TypeProviderConfig) as this =
 
         myType
 
-    let myParamType = 
+    let myParamType =
         let t = ProvidedTypeDefinition(asm, ns, "GenerativeProvider", Some typeof<obj>, isErased=false)
         t.DefineStaticParameters( [ProvidedStaticParameter("Count", typeof<int>)], fun typeName args -> createType typeName (unbox<int> args.[0]))
         t
     do
         this.AddNamespace(ns, [myParamType])
 
+let getLocKeys xaml =
+    let systemNamespaceString = "clr-namespace:System;assembly=System.Runtime"
+    let stringElementName = "String"
+
+    let xamlNamespaceString = "http://schemas.microsoft.com/winfx/2006/xaml"
+    let keyAttributeName = "Key"
+
+    let document = XDocument.Parse(xaml)
+
+    let systemNamespace = XNamespace.Get systemNamespaceString
+    let qualifiedStringElementName = systemNamespace + stringElementName
+
+    let xamlNamespace = XNamespace.Get xamlNamespaceString
+    let qualifiedKeyAttributeName = xamlNamespace + keyAttributeName
+
+    document.Root.Descendants(qualifiedStringElementName)
+    |> Seq.map (fun (element: XElement) -> element.Attribute(qualifiedKeyAttributeName).Value)
+    |> Seq.toList
+
+[<TypeProvider>]
+type LocalizationKeyProvider (config : TypeProviderConfig) as this =
+    inherit TypeProviderForNamespaces (config, assemblyReplacementMap=[("TeaDriven.AvaloniaLocalization.FSharp.DesignTime", "TeaDriven.AvaloniaLocalization.FSharp.Runtime")])
+
+    let ns = "TeaDriven.AvaloniaLocalization.FSharp"
+    let asm = Assembly.GetExecutingAssembly()
+
+    // check we contain a copy of runtime files, and are not referencing the runtime DLL
+    do assert (typeof<DataSource>.Assembly.GetName().Name = asm.GetName().Name)
+
+    let createType typeName (xamlFileName: string) =
+        let asm = ProvidedAssembly()
+        let myType = ProvidedTypeDefinition(asm, ns, typeName, Some typeof<obj>, isErased=false)
+
+        let locKeys = File.ReadAllText xamlFileName |> getLocKeys
+        for key in locKeys do
+            let prop = ProvidedProperty(key.Replace(".", ""), typeof<string>, getterCode = (fun args -> <@@ key @@>), isStatic = true)
+            myType.AddMember(prop)
+
+        asm.AddTypes [ myType ]
+
+        myType
+
+    let myParamType =
+        let t = ProvidedTypeDefinition(asm, ns, "LocKeys", Some typeof<obj>, isErased=false)
+        t.DefineStaticParameters( [ProvidedStaticParameter("FileName", typeof<string>)], fun typeName args -> createType typeName (unbox<string> args.[0]))
+        t
+    do
+        this.AddNamespace(ns, [myParamType])
